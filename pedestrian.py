7120500ffe77395f948941948b1d4ebba4a54b4f
Eric Olson <eric.olson.411@gmail.com>
2017-11-24 21:16:38 -0700

    pedestrian scaffold

--- pedestrian.py
+++ pedestrian.py
@@ -1,5 +1,27 @@
 
 class Pedestrian:
-    def __init__(self, speed):
-        self.speed = speed
+    def __init__(self, num, prng, current_time_prng):
+        self.num = num
 
+        self.arrival = current_time
+
+        self.speed = prng()
+
+        print "[PED]  created with id: {}, speed: {}".format(self.num, self.speed)
+
+    def calc_travel_time(self, distance):
+        # calculate expected travel time
+
+        # return calculated time
+        return
+
+    def calc_crosswalk_time(self, dist_to_crosswalk):
+        # calculate when ped will arrive at crosswalk
+
+        # return expected time
+        return
+
+    def light_red(self, current_time, t_red):
+        # when light turns red, ped can likely cross
+
+        # return delay time? if crossed?
