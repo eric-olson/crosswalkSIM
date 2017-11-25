4f6e8ea76bfe0f8971e1a20249048a51910476d6
Eric Olson <eric.olson.411@gmail.com>
2017-11-24 21:16:18 -0700

    auto cleanup & verbosity

--- auto.py
+++ auto.py
@@ -17,7 +17,10 @@ class Auto:
         self.accel = accel
 
         # print debug message
-        print "[AUTO] created with id: {}, speed: {}".format(self.num, self.speed)
+        print "[AUTO] created with id: {}, speed: {}, accel: {}, delayed: {}".format(self.num,
+                                                                                     self.speed,
+                                                                                     self.accel,
+                                                                                     self.delayed)
 
     def calc_travel_time(self, distance):
         # calculate expected travel time
